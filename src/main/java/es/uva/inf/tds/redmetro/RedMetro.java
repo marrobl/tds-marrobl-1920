b68e05ba02f4448c335deaff1030d648e7c6f91f
Maria Robles <maria.robles.blanco@alumnos.uva.es>
2019-12-30 17:43:16 +0100

    Añadidas las clases RedMetro.java y la clase de tests RedMetroTest.java, creado tambien el test para inicializar una red de metro y comprobar que se ha creado bien asi como el stub autogenerado

---
+++ src/main/java/es/uva/inf/tds/redmetro/RedMetro.java
@@ -0,0 +1,16 @@
+package es.uva.inf.tds.redmetro;
+
+import java.util.ArrayList;
+
+public class RedMetro {
+
+	public RedMetro(ArrayList<Linea> lineas) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Linea[] getLineas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/es/uva/inf/tds/redmetro/RedMetroTDDTest.java
@@ -0,0 +1,38 @@
+package es.uva.inf.tds.redmetro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import es.uva.inf.maps.CoordenadasGPS;
+
+public class RedMetroTDDTest {
+	
+	@Test
+	@Tag("TDD")
+	void RedMetroTest() {
+		CoordenadasGPS coordEntrada1 = new CoordenadasGPS("041°38'06\"N","135°05'59\"E");
+		CoordenadasGPS coordSalida1 = new CoordenadasGPS("045°38'06\"N","132°05'59\"E");
+		CoordenadasGPS[] coordenadasInicial = {coordEntrada1, coordSalida1};
+ 		Estacion estacionInicial = new Estacion("Primera Estacion", coordenadasInicial);
+		CoordenadasGPS coordEntrada2 = new CoordenadasGPS("058°38'06\"N","136°05'59\"E");
+		CoordenadasGPS coordSalida2 = new CoordenadasGPS("064°38'06\"N","135°05'59\"E");
+		CoordenadasGPS[] coordenadasFinal = {coordEntrada2, coordSalida2};
+ 		Estacion estacionFinal = new Estacion("Segunda Estacion", coordenadasFinal);
+ 		Estacion[] estaciones = {estacionInicial, estacionFinal};
+ 		Linea lineaPrimera = new Linea(1,"rojo",estaciones);
+ 		Linea lineaSegunda = new Linea(2, "azul", estaciones);
+ 		Linea lineaTercera = new Linea(3, "verde", estaciones);
+		ArrayList<Linea> lineas = new ArrayList<>();
+		lineas.add(lineaPrimera);
+		lineas.add(lineaSegunda);
+		lineas.add(lineaTercera);
+		RedMetro redMetro = new RedMetro(lineas);
+		assertNotNull(redMetro.getLineas());
+		assertArrayEquals(lineas.toArray(),redMetro.getLineas());
+	}
+
+}

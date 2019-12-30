c30fd18066e38380057ecc394c9d1ff1943743fd
Maria Robles <maria.robles.blanco@alumnos.uva.es>
2019-12-30 18:18:41 +0100

    Añadida la documentacion a los metodos de la clase RedMetro autogenerados

--- src/main/java/es/uva/inf/tds/redmetro/RedMetro.java
+++ src/main/java/es/uva/inf/tds/redmetro/RedMetro.java
@@ -4,10 +4,22 @@ import java.util.ArrayList;
 
 public class RedMetro {
 
+	/**
+	 * Inicializador de una red de metro, formada por varias lineas. 
+	 * Una red de metro tiene que tener al menos dos lineas. 
+	 * Una linea esta univocamente identificada en la red por un numero consecutivo
+	 * y un color que no puede coincidir con colores de otras lineas de esa red
+	 * 
+	 * @param lineas conjunto de Lineas que forman la red
+	 */
 	public RedMetro(ArrayList<Linea> lineas) {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Consulta las lineas que pertenecen a la red de metro, ya esten desactivadas o eliminadas
+	 * @return lineas que forman la red
+	 */
 	public Linea[] getLineas() {
 		// TODO Auto-generated method stub
 		return null;
